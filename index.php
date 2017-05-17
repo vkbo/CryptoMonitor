b93edfb613c1de59ff9baf853bdf7729c7e1e0c8
Veronica K. B. Olsen <code@vkbo.net>
2017-05-17 19:39:52 +0200

    Added basic files

---
+++ index.php
@@ -0,0 +1,20 @@
+<?php
+    /**
+     *  Crypto Monitor – Index File
+     * =============================
+     *  Created 2017-05-17
+     */
+?><!DOCTYPE html>
+
+<html>
+    <link rel="stylesheet" href="css/normalize.css" type="text/css" media="all">
+    <link rel="stylesheet" href="css/styles.css" type="text/css" media="all">
+<head>
+
+</head>
+
+<body>
+    Hello Kitty!
+</body>
+
+</html>
