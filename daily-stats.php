d8df4969c6f4b560d73de32e4ccc7e08f955662a
Veronica K. B. Olsen <code@vkbo.net>
2017-05-19 18:56:03 +0200

    Added daily stats script and updated hourly script

--- daily-stats.php
+++ daily-stats.php
@@ -1,7 +1,7 @@
 <?php
    /**
-    *  Crypto Monitor – Hourly Stats Script
-    * ======================================
+    *  Crypto Monitor – Daily Stats Script
+    * =====================================
     *  Created 2017-05-19
     */
 
@@ -13,8 +13,8 @@
 
     $oPools = $oDB->query("SELECT * FROM pools WHERE Active = 1");
 
-    function getTimeStamp()    {return date("[Y-m-d H:i:s]",time());};
-    function roundHour($iTime) {return strtotime(date("Y-m-d H:00:00",$iTime));};
+    function getTimeStamp()   {return date("[Y-m-d H:i:s]",time());};
+    function roundDay($iTime) {return strtotime(date("Y-m-d 00:00:00",$iTime));};
 
     while($aPool = $oPools->fetch_assoc()) {
 
@@ -32,27 +32,27 @@
         while($aWallet = $oWallet->fetch_assoc()) {
 
             // Calculate Hourly Averages
-            echo getTimeStamp()." Calculating hourly averages ";
+            echo getTimeStamp()." Calculating daily averages ";
 
             $SQL = sprintf(
                "SELECT TimeStamp
-                FROM mining_hourly
+                FROM mining_daily
                 WHERE WalletID = '%s' AND PoolID = '%s'
                 ORDER BY TimeStamp DESC
                 LIMIT 0, 1",
                 $aWallet["ID"],
                 $aPool["ID"]
             );
-            $oHourly = $oDB->query($SQL);
-            if($oHourly->num_rows > 0) {
-                $aTemp = $oHourly->fetch_assoc();
-                $iLastHour = strtotime($aTemp["TimeStamp"])+3600;
+            $oDaily = $oDB->query($SQL);
+            if($oDaily->num_rows > 0) {
+                $aTemp = $oDaily->fetch_assoc();
+                $iLastDay = strtotime($aTemp["TimeStamp"])+86400;
             } else {
-                $iLastHour = roundHour(time()-7*86400);
+                $iLastDay = roundDay(time()-7*86400);
             }
-            echo "since ".date("Y-m-d H:i:s",$iLastHour)."\n";
-            $iCurrHour = roundHour(time());
-            for($iStart = $iLastHour; $iStart < $iCurrHour; $iStart += 3600) {
+            echo "since ".date("Y-m-d H:i:s",$iLastDay)."\n";
+            $iCurrDay = roundDay(time());
+            for($iStart = $iLastDay; $iStart < $iCurrDay; $iStart += 86400) {
                 echo getTimeStamp()." Averaging for ".date("Y-m-d H:i:s",$iStart)."\n";
                 $SQL = sprintf(
                    "SELECT
@@ -67,19 +67,19 @@
                     $aWallet["ID"],
                     $aPool["ID"],
                     date("Y-m-d-H-i-s",$iStart),
-                    date("Y-m-d-H-i-s",$iStart+3600)
+                    date("Y-m-d-H-i-s",$iStart+86400)
                 );
-                $oHourly = $oDB->query($SQL);
-                if($oHourly->num_rows > 0) {
-                    $aHourly = $oHourly->fetch_assoc();
-                    $SQL  = "INSERT INTO mining_hourly (TimeStamp,WalletID,PoolID,Hashes,Balance,HashRate,Entries) VALUES (";
+                $oDaily = $oDB->query($SQL);
+                if($oDaily->num_rows > 0) {
+                    $aDaily = $oDaily->fetch_assoc();
+                    $SQL  = "INSERT INTO mining_daily (TimeStamp,WalletID,PoolID,Hashes,Balance,HashRate,Entries) VALUES (";
                     $SQL .= "'".date("Y-m-d-H-i-s",$iStart)."',";
                     $SQL .= "'".$aWallet["ID"]."',";
                     $SQL .= "'".$aPool["ID"]."',";
-                    $SQL .= "'".$aHourly["Hashes"]."',";
-                    $SQL .= "'".$aHourly["Balance"]."',";
-                    $SQL .= "'".$aHourly["HashRate"]."',";
-                    $SQL .= "'".$aHourly["Entries"]."')";
+                    $SQL .= "'".$aDaily["Hashes"]."',";
+                    $SQL .= "'".$aDaily["Balance"]."',";
+                    $SQL .= "'".$aDaily["HashRate"]."',";
+                    $SQL .= "'".$aDaily["Entries"]."')";
                     $oDB->query($SQL);
                 }
 
